02ff7467880b5d8acd5613247a2865ff07712f2f
Xehny <nathan@pook.nz>
2023-04-12 20:06:10 +1200

    Added rack generation

--- Wimsdata.java
+++ Wimsdata.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Wimsdata {
+    private static int bottles;
+    private static int racks;
+    private static int imp = 1000;
+    private static int exp = 1000;
+
     public static void main (String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: Wimsdata <bottles> <racks>");
             return;
         }
-        
-        int bottles, racks;
 
         try {
             bottles = Integer.parseInt(args[0]);
@@ -24,29 +27,29 @@ class Wimsdata {
             return;
         }
 
-        genBottles(bottles, racks);
+        genBottles();
+        genRacks();
     }
 
-    private static void genBottles(int bottles, int racks) {
+    private static void genBottles() {
         try{
             BufferedWriter writer = new BufferedWriter(new FileWriter("bottles.csv"));
             Random rand = new Random();
 
-            int year, volume, variety, branch, rack, imp = 1000, exp = 1000;
+            int year, volume, variety, branch;
             double alc, list, retail;
 
             for (int i = 0; i < bottles; i++) {
                 year = 1990 + rand.nextInt(30);
                 volume = randVolume(rand.nextInt(10000));
-                alc = Math.round((3 + (rand.nextDouble() * 19)) * 100) / 100;
-                list = Math.round((5 + (rand.nextDouble() * 120)) * 100) / 100;
-                retail = Math.round((5 + (list * 1.2)) * 100) / 100;
+                alc = Math.round((3 + (rand.nextDouble() * 17)) * 100.0) / 100.0;
+                list = Math.round((5 + (rand.nextDouble() * 120)) * 100.0) / 100.0;
+                retail = Math.round((5 + (list * 1.2)) * 100.0) / 100.0;
                 variety = 1000 + rand.nextInt(32);
-                branch = 1000 + rand.nextInt(120);
-                rack = 1000 + rand.nextInt(racks);
+                branch = 1000 + rand.nextInt(120);;
 
-                writer.write(String.valueOf(year) + ',' + String.valueOf(volume) + ',' + String.valueOf(alc) + ',' + String.valueOf(list) + ',' + String.valueOf(retail) + ',' + String.valueOf(variety) + ',' + String.valueOf(branch) + ',' + String.valueOf(rack) + ',' + String.valueOf(imp) + ',');
-                
+                writer.write(String.valueOf(year) + ',' + String.valueOf(volume) + ',' + String.valueOf(alc) + ',' + String.valueOf(list) + ',' + String.valueOf(retail) + ',' + String.valueOf(variety) + ',' + String.valueOf(branch) + ',' + String.valueOf(imp) + ',');
+
                 if (rand.nextDouble() < 0.05) {
                     writer.write("NULL");
                 }
@@ -58,9 +61,45 @@ class Wimsdata {
                     writer.write(String.valueOf(exp));
                 }
 
+                if (rand.nextDouble() < 0.15) {
+                    imp += 1;
+                }
+
                 writer.newLine();
             }
             
+            writer.close();
+        }
+        catch (Exception ex) {
+            System.err.println(ex);
+            System.exit(0);
+        }
+    }
+
+    private static void genRacks() {
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("racks.csv"));
+            Random rand = new Random();
+
+            String name;
+            int length, chill;
+
+            length = (int) Math.sqrt((bottles * 0.10) / racks);
+
+            for (int i = 0; i < racks; i++) {
+                name = "\'Rack " + String.valueOf(i + 1) + "\'";
+
+                if (rand.nextDouble() < 0.15) {
+                    chill = 1;
+                }
+                else {
+                    chill = 0;
+                }
+
+                writer.write(name + ',' + String.valueOf(length) + ',' + String.valueOf(length) + ',' + String.valueOf(chill));
+                writer.newLine();
+            }
+
             writer.close();
         }
         catch (Exception ex) {
