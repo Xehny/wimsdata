672e67435d0bab00a1b648c5180f0a8661e87e41
Xehny <nathan@pook.nz>
2023-04-12 15:16:05 +1200

    Arg validation

--- Wimsdata.java
+++ Wimsdata.java
@@ -1,5 +1,30 @@
+import java.io.*;
+
 class Wimsdata {
     public static void main (String[] args) {
-        System.out.println("Wimsdata");
+        if (args.length != 4) {
+            System.out.println("Usage: Wimsdata <bottles> <racks> <imports> <exports>");
+            return;
+        }
+        
+        int bottles, racks, imports, exports;
+
+        try {
+            bottles = Integer.parseInt(args[0]);
+            racks = Integer.parseInt(args[1]);
+            imports = Integer.parseInt(args[2]);
+            exports = Integer.parseInt(args[3]);
+
+            if (bottles < 1 || racks < 1 || imports < 1 || exports < 1) {
+                System.err.println("Error: Arguments must be greater than 0");
+                return;
+            }
+        }
+        catch (Exception ex) {
+            System.err.println("Error: Arguments must be integers");
+            return;
+        }
+
+        System.out.println("Correct args!");
     }
 }
